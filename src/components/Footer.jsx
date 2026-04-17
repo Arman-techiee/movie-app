819038af01d5e17b116c20de54c613706859a80d
armanroot <arman.techiee@gmail.com>
2026-04-17 15:19:05 +0545

    chore: update GitHub username to armancore

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,7 +30,7 @@ export default function Footer() {
               Project
             </span>
             <a
-              href="https://github.com/armanroot/movie-app.git"
+              href="https://github.com/armancore/movie-app.git"
               target="_blank"
               rel="noreferrer"
               className="inline-flex items-center gap-2 text-cinema-text hover:text-cinema-gold transition-colors"
